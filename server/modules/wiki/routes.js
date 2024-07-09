83d42d65b77d2b53c0d262b7095db5b068459fb1
Hassan Amin <hassanmamin994@gmail.com>
2024-07-09 23:43:28 +0300

    feat: add rate limit on wiki search requests

--- server/modules/wiki/routes.js
+++ server/modules/wiki/routes.js
@@ -1,3 +1,4 @@
+import { rateLimit } from 'express-rate-limit'
 import controller from './controller';
 import isAuthenticated from '../shared/middlewares/isAuthenticated';
 import { saveTemplate } from '../shared/middlewares/saveTemplate';
@@ -5,14 +6,26 @@ import { uploadFileToWikiCommons } from '../shared/middlewares/wikiUpload';
 import { checkEditableArticle } from '../shared/middlewares/checkEditableArticle';
 import uploadLocal from '../shared/middlewares/uploadLocal';
 
+const searchRateLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 12, // limit each IP to 10 requests per windowMs
+  standardHeaders: 'draft-7',
+})
+
+const summaryRateLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 12, // limit each IP to 10 requests per windowMs
+  standardHeaders: 'draft-7',
+})
+
 const mount = function(router) {
   // ========== Search
-  router.get('/search', controller.searchWikiArticles);
+  router.get('/search', searchRateLimiter, controller.searchWikiArticles);
 
-  router.get('/wikimediaCommons/images', controller.searchWikiCommonsImages);
-  router.get('/wikimediaCommons/gifs', controller.searchWikiCommonsGifs);
-  router.get('/wikimediaCommons/videos', controller.searchWikiCommonsVideos)
-  router.get('/wikimediaCommons/categories', controller.searchWikiCommonsCategories);
+  router.get('/wikimediaCommons/images', searchRateLimiter, controller.searchWikiCommonsImages);
+  router.get('/wikimediaCommons/gifs', searchRateLimiter, controller.searchWikiCommonsGifs);
+  router.get('/wikimediaCommons/videos', searchRateLimiter, controller.searchWikiCommonsVideos)
+  router.get('/wikimediaCommons/categories', searchRateLimiter, controller.searchWikiCommonsCategories);
 
   // ============== upload image url to slide
   router.post('/article/imageUpload', checkEditableArticle, controller.uploadImageURLToSlide);
@@ -28,14 +41,14 @@ const mount = function(router) {
   router.get('/article', controller.getArticleByTitle);
 
   // ============== Fetch article summary by title
-  router.get('/article/summary', controller.getArticleSummaryByTitle);
+  router.get('/article/summary', summaryRateLimiter, controller.getArticleSummaryByTitle);
 
   // ============== Convert wiki to video wiki
   router.get('/convert', controller.convertWikiToVideowiki);
 
   router.get('/updateArticle', controller.updateVideowikiArticle);
   // ================ Get infobox
-  router.get('/infobox', controller.getArticleInfobox);
+  router.get('/infobox', summaryRateLimiter, controller.getArticleInfobox);
 
   // ============== Get commons video url by it's name
   router.post('/commons/video_by_name', controller.getVideoByName);
